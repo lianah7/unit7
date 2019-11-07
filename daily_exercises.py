a1df534b181ddcd32658b9d27f2295c5e2b29c6b
liana <hilllia@ssfs.org>
2019-11-07 11:20:16 -0500

    worked on unit seven daily exercises

--- daily_exercises.py
+++ daily_exercises.py
@@ -1,45 +1,67 @@
 # by Liana Hill
 # last updated November 6, 2019
 # unit 7 daily exercises
+#
+# # problem one
+# original = "abcdefghij"
+# first = original[0:3]
+# last = original[3:]
+# final = first + last
+# print(final)
+#
+#
+# # problem two
+# words = "Python"
+# for character in words:
+#     print(character)
+#
+#
+# # problem three
+# def without_end(word):
+#     word = word[1: -1]
+#     return word
+#
+#
+# print(without_end("Hello"))
+# print(without_end("python"))
+# print(without_end("coding"))
+#
+#
+# # problem four
+# def longest(list_words):
+#     longest_word = ""
+#     for x in list_words:
+#         if len(x) > len(longest_word):
+#             longest_word = x
+#     return longest_word
+#
+#
+# print(longest(["cat", "dog", "horse"]))
+#
+#
+# # problem five
+# def upper_lower():
+#     user_word = input("Enter a word")
+#     print(user_word.upper())
+#     print(user_word.lower())
+#
+# upper_lower()
 
 
-# problem one
-original = "abcdefghij"
-first = original[0:3]
-last = original[3:]
-final = first + last
-print(final)
+# problem six
+def title_case():
+    user_sentence = input("Please enter a sentence")
+    title_sentence = user_sentence.split(" ")
+    capital = []
+    for character in title_sentence:
+        capitalized_word = character[0].upper() + character[1:]
+        capital.append(capitalized_word)
+    joined = " ".join(capital)
+    return joined
 
 
-# problem two
-words = "Python"
-for character in words:
-    print(character)
+print(title_case())
 
-
-# problem three
-def without_end(word):
-    word = word[1: -1]
-    return word
-
-
-print(without_end("Hello"))
-print(without_end("python"))
-print(without_end("coding"))
-
-
-# problem four
-def longest(list_words):
-    longest_word = ""
-    for x in list_words:
-        if len(x) > len(longest_word):
-            longest_word = x
-    return longest_word
-
-
-print(longest(["cat", "dog", "horse"]))
-
-# problem five
-def upper_lower():
-    user_word = input("Enter a word")
-
+# problem seven
+def replace_four(your_sentence):
+    if len(word)
