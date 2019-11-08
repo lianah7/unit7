98cf71bf71eecce690eb680957ede024b0beb96e
liana <hilllia@ssfs.org>
2019-11-08 12:05:12 -0500

    worked on unit seven daily exercises

--- daily_exercises.py
+++ daily_exercises.py
@@ -46,22 +46,41 @@
 #     print(user_word.lower())
 #
 # upper_lower()
+#
+#
+# # problem six
+# def title_case():
+#     user_sentence = input("Please enter a sentence")
+#     title_sentence = user_sentence.split(" ")
+#     capital = []
+#     for character in title_sentence:
+#         capitalized_word = character[0].upper() + character[1:]
+#         capital.append(capitalized_word)
+#     joined = " ".join(capital)
+#     return joined
+#
+#
+# print(title_case())
+#
+#
+# # problem seven
+# def replace_four(your_sentence):
+#     sentence = your_sentence.split(" ")
+#     list_sentence = []
+#     for character in sentence:
+#         if len(character) == 4:
+#             new_word = "#$%@"
+#             list_sentence.append(new_word)
+#         else:
+#             list_sentence.append(character)
+#     new_sentence = " ".join(list_sentence)
+#     return new_sentence
+#
+#
+# print(replace_four("Suzie is a nice person"))
 
 
-# problem six
-def title_case():
-    user_sentence = input("Please enter a sentence")
-    title_sentence = user_sentence.split(" ")
-    capital = []
-    for character in title_sentence:
-        capitalized_word = character[0].upper() + character[1:]
-        capital.append(capitalized_word)
-    joined = " ".join(capital)
-    return joined
+# problem eight
+def bubble_sort(names):
+    for x in range(len(names)):
 
-
-print(title_case())
-
-# problem seven
-def replace_four(your_sentence):
-    if len(word)
